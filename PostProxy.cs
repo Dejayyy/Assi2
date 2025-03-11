46d13e2129d55434bd15d86efe2c43e1f20d0583
billy mouhtouris <bmouhtouris@hotmail.com>
2025-03-11 12:06:35 -0400

    Proxy done?

--- PostProxy.cs
+++ PostProxy.cs
@@ -4,19 +4,93 @@ namespace Assi2
 {
     class PostProxy : Content
     {
+        private Post _realPost;
+        private bool _isDownloaded;
+
+        public PostProxy()
+        {
+            _isDownloaded = false;
+            _realPost = null;
+        }
+
+        public void Download()
+        {
+            if (!_isDownloaded)
+            {
+                _realPost = new FancyPost();
+                _isDownloaded = true;
+            }
+        }
+
+        public bool IsDownloaded()
+        {
+            return _isDownloaded;
+        }
+
+        public void SetTitle(string title)
+        {
+            if (!_isDownloaded)
+            {
+                Console.WriteLine("Error: Post not downloaded yet!");
+                return;
+            }
+
+            if (_realPost != null)
+            {
+                _realPost.Title = title;
+            }
+        }
+
+        public void SetBody(string body)
+        {
+            if (!_isDownloaded)
+            {
+                Console.WriteLine("Error: Post not downloaded yet!");
+                return;
+            }
+
+            if (_realPost != null)
+            {
+                _realPost.Body = body;
+            }
+        }
+
         public override Content Clone()
         {
-            throw new NotImplementedException();
+            PostProxy newProxy = new PostProxy();
+            if (_isDownloaded && _realPost != null)
+            {
+                newProxy._realPost = (Post)_realPost.Clone();
+                newProxy._isDownloaded = true;
+            }
+            return newProxy;
         }
 
         protected override string GetPrintableBody()
         {
-            throw new NotImplementedException();
+            if (!_isDownloaded)
+            {
+                return "[Post not downloaded yet]";
+            }
+            if (_realPost != null)
+            {
+                return _realPost.Body;
+            }
+            return "Unable to access post body";
+
         }
 
         protected override string GetPrintableTitle()
         {
-            throw new NotImplementedException();
+            if (!_isDownloaded)
+            {
+                return "[Post not downloaded yet]";
+            }
+            if (_realPost != null)
+            {
+                return _realPost.Title;
+            }
+            return "Unable to access post title";
         }
     }
 }
